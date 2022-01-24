639b4360c8f4a1cea7ea8cc38263ea66b1ffdf40
shuiwudengli <96819893+shuiwudengli@users.noreply.github.com>
2022-01-24 20:24:39 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -19,13 +19,13 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "Aria"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
+author = "亚利亚社长"
+email = "2234251558@qq.com"
 author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
+description = "单手领航员"
 key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
